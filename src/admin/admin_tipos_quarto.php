d8985f5d2925148bdf8eabfe9c3ffdfce6cc96b0
Deuwer <deuwerrabelais8@gmail.com>
2026-06-03 18:52:47 +0100

    Início da página Home do admin e criação da página tipos de quarto
    Apenas foi iniciada a página Home do administrador, com o conteúdo ainda a pensar e a construção ainda vazia da tabela da página tipos de quarto

---
+++ src/admin/admin_tipos_quarto.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tipos de Quarto</title>
+</head>
+<body>
+    <div>
+        <table>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Tipo</th>
+                    <th>Capacidade</th>
+                    <th>Preço</th>
+                    <th>Peq. Almoço</th>
+                    <th>Editar</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td></td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</body>
+</html>
