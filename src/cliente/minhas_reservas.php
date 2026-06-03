69796a341e66f611372c1229482cbb87e56937bf
Deuwer <deuwerrabelais8@gmail.com>
2026-06-03 18:33:07 +0100

    criação da página minhas reservas
    Foi criada a primeira versão vazia da página minhas reservas, o menu principal ainda não se encontra nas outras páginas mas será brevemente colocado

---
+++ src/cliente/minhas_reservas.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Minhas Reservas</title>
+</head>
+<body>
+    <h1>As minhas Reservas</h1>
+
+    <div>
+        <table>
+            <thead>
+                <tr>
+                    <th>Check-in</th>
+                    <th>Check-out</th>
+                    <th>Tipo de Quarto</th>
+                    <th>Total a pagar</th>
+                    <th>Editar</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td></td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</body>
+</html>
