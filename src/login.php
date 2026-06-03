7c4be81d505b7d08e3087aa375fbb30fcabc7bff
Deuwer <deuwerrabelais8@gmail.com>
2026-06-03 13:09:11 +0100

    Criação da página login
    Primeira versão da página login sem css, e em preparação para a página registar

---
+++ src/login.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <h1>Akime Hotel</h1>
+    <div>
+        <h2>Entrar na sua Conta </h2>
+        <form action="">
+            <label for="email">Email</label>
+            <input type="email" placeholder="email@exemplo.com" id="email" required>
+            <br>
+
+            <label for="password">Password</label>
+            <input type="password" placeholder="********" id="password" required>
+            <br>
+
+            <button type="submit">Entrar</button>
+            <br>
+
+            <span>Não tens conta aqui? <a href="registar.php">Regista-te aqui</a></span>
+        </form>
+    </div>
+</body>
+</html>
