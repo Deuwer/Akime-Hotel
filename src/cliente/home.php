3de529aa8fa61cfea0fc76b896b0bab123b6b344
Deuwer <deuwerrabelais8@gmail.com>
2026-06-03 13:55:08 +0100

    Criação da página home do cliente
    Esta a primeira versão da página home do cliente, onde as "caixas" de Nova Reserva. e Minhas Reservas serão estilizadas e possivelmente clicáveis

---
+++ src/cliente/home.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home Cliente</title>
+</head>
+<body>
+    <div>
+        <h1>Akime Hotel</h1>
+    </div>
+
+    <header>
+        <ul>
+            <li>Home</li>
+            <li>Nova Reserva</li>
+            <li>Minhas Reservas</li>
+        </ul>
+    </header>
+
+    <div>
+        <div>
+            <h3>Nova Reserva</h3>
+        </div>
+    </div>
+
+    <div>
+        <div>
+            <h3>Minhas Reservas</h3>
+        </div>
+    </div>
+</body>
+</html>
