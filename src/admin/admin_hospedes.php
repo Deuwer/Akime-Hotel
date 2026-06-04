c195ebb5140e3b39c4f0de7fdb3ee4f385dffafe
Deuwer <deuwerrabelais8@gmail.com>
2026-06-04 19:19:43 +0100

    Esta é para a página de gestão de hóspedes
    O resto está a ser desenvolvido

--- src/admin/admin_hospedes.php
+++ src/admin/admin_hospedes.php
@@ -1,3 +1,19 @@
+<?php
+
+session_start();
+require_once "../config/database.php";
+
+$sql = "SELECT *
+        FROM Hospede
+        ORDER BY host_nome ASC";
+
+$stmt = $pdo->prepare($sql);
+$stmt->execute();
+
+$hospedes = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+?>
+
 <!DOCTYPE html>
 <html lang="pt">
 <head>
@@ -6,25 +22,37 @@
     <title>Hóspedes</title>
 </head>
 <body>
-    <h2>Hóspedes</h2>
 
-    <div>
-        <table>
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Nome</th>
-                    <th>Documento</th>
-                    <th>NIF</th>
-                    <th>Estado</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td></td>
-                </tr>
-            </tbody>
-        </table>
-    </div>
+<h2>Hóspedes</h2>
+
+<table>
+    <tr>
+        <th>ID</th>
+        <th>Nome</th>
+        <th>Email</th>
+        <th>Documento</th>
+        <th>NIF</th>
+        <th>Estado</th>
+    </tr>
+
+    <?php foreach ($hospedes as $hospede) { ?>
+
+        <tr>
+            <td><?php echo $hospede["host_id"]; ?></td>
+            <td><?php echo $hospede["host_nome"]; ?></td>
+            <td><?php echo $hospede["host_email"]; ?></td>
+            <td><?php echo $hospede["host_documento"]; ?></td>
+            <td><?php echo $hospede["host_nif"]; ?></td>
+            <td><?php echo $hospede["host_estado"]; ?></td>
+        </tr>
+
+    <?php } ?>
+
+</table>
+
+<br>
+
+<a href="admin_home.php">Voltar</a>
+
 </body>
 </html>
