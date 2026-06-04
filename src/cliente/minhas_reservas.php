e86639878b9969aed3f2d0be792e1af4a2ea781f
Deuwer <deuwerrabelais8@gmail.com>
2026-06-04 14:07:41 +0100

    Preparação para a lista que será visualizada na ainterface das reservas
    Tenho como objetivo mostrar os registos de reserva

--- src/cliente/minhas_reservas.php
+++ src/cliente/minhas_reservas.php
@@ -7,6 +7,17 @@ if (!isset($_SESSION["host_id"])) {
     exit();
 }
 
+$host_id = $_SESSION["host_id"];
+$sql = "SELECT *
+        FROM Reserva
+        WHERE res_host_id = ?
+        ORDER BY res_inicio DESC";
+
+$stmt = $pdo->prepare($sql);
+$stmt->execute([$host_id]);
+
+$reservas = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
 ?>
 
 <!DOCTYPE html>
